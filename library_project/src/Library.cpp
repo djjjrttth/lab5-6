da0d636b481c50705adbfb38694e4b6f609954d6
djjjrtth <klikat08@gmail.com>
2025-12-12 19:43:43 +0300

    added loading from file

--- library_project/src/Library.cpp
+++ library_project/src/Library.cpp
@@ -9,6 +9,7 @@ Library::Library(std::vector<Book> _books, std::vector<User> _users, std::string
     users = _users;
     dataFile = _dataFile;
 
+    loadFromFile();
     /*std::ifstream in;
     in.open(dataFile);
 
@@ -175,7 +176,116 @@ void Library::saveToFile(){
         out << endl;
         out << "MaxBooks: " << users[i].getMaxBooksAllowed() << endl;
     }
+
+    out.close();
 }
 void Library::loadFromFile(){
 
+    std::ifstream in;
+    in.open(dataFile);
+
+    std::string type = "BOOK";
+    std::string title;
+    std::string author;
+    std::string str_year;
+    std::string isbn;
+    std::string isAvailable;
+    std::string borrowedBy;
+    int year;
+    bool isAvail;
+    std::string name;
+    std::string userId;
+    std::string borrowedBooks;
+    
+    std::string maxBooksAllowed;
+    int maxBooks;
+    
+    while (type == "BOOK"){
+        
+        getline(in, type);
+        if (type == "---USERS---") break;
+        else{
+        getline(in, title);
+        getline(in, author);
+        getline(in, str_year);
+        getline(in, isbn);
+        getline(in, isAvailable);
+        getline(in, borrowedBy);
+
+    
+        
+        title = title.substr(7, title.size()-7);
+       
+        author = author.substr(8, author.size()-8);
+        
+        int year = std::stoi(str_year.substr(6, str_year.size()-6));
+        isbn = isbn.substr(6, isbn.size()-6);
+        isAvailable = isAvailable.substr(8, isAvailable.size()-8);
+        if (isAvailable == "yes" || isAvailable == "YES")
+            isAvail = true;
+        else if (isAvailable == "no" || isAvailable == "NO"){
+            isAvail = false;
+        }
+        
+        borrowedBy = borrowedBy.substr(12, borrowedBy.size()-12);
+        
+
+        Book b = Book(title, author, year, isbn, isAvail, borrowedBy);
+        addBook(b);
+    }
+    }
+    
+    
+   while((type == "---USERS---" || type == "USER")){
+        
+        getline(in, type);
+        if (type != "USER") break;
+        getline(in, name);
+        getline(in, userId);
+        getline(in, borrowedBooks);
+        getline(in, maxBooksAllowed);
+        
+        name = name.substr(6, name.size()-6);
+        userId = userId.substr(8, userId.size()-8);
+        borrowedBooks = borrowedBooks.substr(15, borrowedBooks.size()-15);
+        maxBooks = std::stoi(maxBooksAllowed.substr(10, maxBooksAllowed.size()-10));
+
+        //cout << name << " " << userId << " " << borrowedBooks << " " << maxBooksAllowed << endl;
+
+        std::vector<std::string> bb;
+        if (borrowedBooks != " "){
+        
+        
+        int prev = 0;
+
+        borrowedBooks += '|';
+
+        char delim = '|';
+
+        for (int i = 0; i < borrowedBooks.size(); i++){
+            if (borrowedBooks[i] == delim){
+                std::string book = borrowedBooks.substr(prev, i-prev);
+                std::string s = "";
+                for (int i = 0; i < book.size(); i++){
+                    s += book[i];
+                }
+                bb.push_back(s);
+                
+                prev = i;
+            }
+        }
+        
+        }
+        
+        User u = User(name, userId, bb, maxBooks);
+        addUser(u);
+        
+        }
+        
+        
+        
+        //User u = User(name, userId, bb, maxBooksAllowed);
+        //addUser(u);
+    
+    
 }
